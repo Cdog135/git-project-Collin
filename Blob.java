bc98e4722f02763d2095bbb4cf1be6719c3aa52f
Riyan Bilimoria Kadribegovic <130018139+RiyanBK@users.noreply.github.com>
2024-09-29 16:21:50 -0700

    git and blob functionality (#1)
    * wrote all of git project part 1 except for zip compression part
    
    wrote git and blob classes and created a tester for them; haven't completed the zip compression part yet (stretch goal #3)
    
    * tested empty file edge case and worked on zipping file content
    
    worked on stretch goal #3 (not tested yet)
    
    * it all works now somehow

---
+++ Blob.java
@@ -0,0 +1,144 @@
+import java.io.*;
+import java.util.*;
+import java.security.*;
+import java.util.zip.*;
+
+public class Blob {
+    private String file;
+    private String hashedFileContent;
+    private File forObjectsFolder;
+    private String fileContent;
+    public static boolean zip = true; // by default it zips the data
+
+    public Blob(String fileName) {
+        file = fileName;
+        fileContent = getFileContent(file);
+        hashedFileContent = toSHA1();
+        forObjectsFolder = new File("git/objects/" + hashedFileContent);
+        try {
+            if (!forObjectsFolder.exists()) {
+                forObjectsFolder.createNewFile();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        try {
+            FileWriter writer = new FileWriter("git/index/", true);
+            writer.write(hashedFileContent + " " + file + "\n");
+            writer.close();
+
+            FileWriter writer2 = new FileWriter(forObjectsFolder);
+            writer2.write(fileContent);
+            writer2.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public String toSHA1() { // mostly from stack overflow:
+                             // https://stackoverflow.com/questions/4895523/java-string-to-sha1
+        if (zip) { // compress the string (this is pseudocode for now bc havent written it yet)
+            fileContent = compress();
+        }
+
+        String sha1 = "";
+        try {
+            MessageDigest encrypter = MessageDigest.getInstance("SHA-1");
+            encrypter.reset();
+            encrypter.update(fileContent.getBytes("UTF-8"));
+            sha1 = byteToHex(encrypter.digest());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return sha1;
+    }
+
+    public String getFileContent(String fileName) {
+        StringBuilder content = new StringBuilder();
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            while (reader.ready()) {
+                content.append((char) reader.read());
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        String contentString = content.toString();
+        return contentString;
+    }
+
+    private static String byteToHex(final byte[] hash) { // shamelessly copied from stack overflow:
+                                                         // https://stackoverflow.com/questions/4895523/java-string-to-sha1
+        Formatter formatter = new Formatter();
+        for (byte b : hash) {
+            formatter.format("%02x", b);
+        }
+        String result = formatter.toString();
+        formatter.close();
+        return result;
+    }
+
+    public static boolean toggleZip() {
+        if (zip) {
+            zip = false;
+            return false;
+        } else {
+            zip = true;
+            return true;
+        }
+    }
+
+    public static boolean getZip() {
+        return zip;
+    }
+
+    public String compress() {
+        byte[] input; // https://docs.oracle.com/javase/8/docs/api/java/util/zip/Deflater.html
+        byte[] output = new byte[1000];
+        try {
+            // Encode a String into bytes
+            input = fileContent.getBytes("UTF-8");
+
+            // Compress the bytes
+            Deflater compresser = new Deflater();
+            compresser.setInput(input);
+            compresser.finish();
+            //System.out.println (compresser.finished()); //for testing
+            int compressedDataLength = compresser.deflate(output); //not used but may be necessary?
+            compresser.end();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        return new String(output);
+
+        // bad code:
+        // zip fileContent
+        // inspiration:
+        // https://stackoverflow.com/questions/63885313/is-there-a-way-to-zip-a-plain-text-file-from-a-string-in-java
+        // String zippedContent = "";
+        // try {
+        // ZipOutputStream zipped = new ZipOutputStream(new
+        // FileOutputStream(fileContent));
+        // zippedContent = zipped.toString();
+        // zipped.close();
+        // } catch (Exception e) {
+        // e.printStackTrace();
+        // }
+        // return zippedContent;
+
+        /*
+         * StringBuilder sb = new StringBuilder(fileContent);
+         * File f = new File("c:\\payload.zip");
+         * ZipOutputStream out = new ZipOutputStream(new FileOutputStream(f));
+         * ZipEntry e = new ZipEntry("myFile.txt");
+         * out.putNextEntry(e);
+         * byte[] data = sb.toString().getBytes();
+         * out.write(data, 0, data.length);
+         * out.closeEntry();
+         * out.close();
+         */
+    }
+
+}
---
+++ GitTester.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class GitTester {
+    public static void main (String [] args) {
+        Git git = new Git();
+        Blob blob = new Blob ("test.txt");
+        System.out.println (blob.toSHA1());
+        Blob blob2 = new Blob ("test3.txt");
+        System.out.println (blob2.toSHA1());
+        //git.deleteGit();
+        //can comment out above code to make it delete files or not
+    }
+
+}
